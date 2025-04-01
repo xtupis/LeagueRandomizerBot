78b01321a3fc52c9be770d0e4b8d3581d8eafe29
xtupis <profolinm@gmail.com>
2025-04-01 17:42:24 +0300

    TOKEN REMOVED

--- src/main/java/org/xtupis/randomizer/token/TokenPovider.java
+++ src/main/java/org/xtupis/randomizer/token/TokenPovider.java
@@ -1,7 +1,7 @@
 package org.xtupis.randomizer.token;
 
 public class TokenPovider {
-    private static final String TOKEN = "7973540744:AAH7MLEaHeTT526h0KfCI7vtJ__KlJDiFvw";
+    private static final String TOKEN = "YOUR_TOKEN";
 
     public static String getToken() {
         return TOKEN;
